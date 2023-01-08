bb734c4a7cf1de5994022be98f9784ee099a8671
maria <mana.alattas@gmail.com>
2023-01-08 16:39:36 +0300

    test

---
+++ AtoZ/AtoZ/AtoZApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtoZApp.swift
+//  AtoZ
+//
+//  Created by Maria Alattas on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct AtoZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
